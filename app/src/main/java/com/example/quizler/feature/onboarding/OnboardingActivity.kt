4003bc27ee40b9cbce48127c5494e3d67cecb7fd
urkeev14 <uros.veljkovic@gmail.com>
2021-06-04 20:43:55 +0200

    Add icons and drawables

--- app/src/main/java/com/example/quizler/feature/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/quizler/feature/onboarding/OnboardingActivity.kt
@@ -2,6 +2,7 @@ package com.example.quizler.feature.onboarding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizler.R
 
 class OnboardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
