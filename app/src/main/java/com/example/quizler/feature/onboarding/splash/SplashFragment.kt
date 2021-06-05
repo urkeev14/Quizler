889a54042ba00281f5270e10e86c4b2290676007
urkeev14 <uros.veljkovic@gmail.com>
2021-06-05 12:35:25 +0200

    Implement animation on splash screen

--- app/src/main/java/com/example/quizler/feature/onboarding/splash/SplashFragment.kt
+++ app/src/main/java/com/example/quizler/feature/onboarding/splash/SplashFragment.kt
@@ -1,15 +1,16 @@
 package com.example.quizler.feature.onboarding.splash
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.quizler.R
+import com.example.quizler.databinding.SplashFragmentBinding
 
 class SplashFragment : Fragment() {
 
+    private lateinit var binding: SplashFragmentBinding
     private lateinit var viewModel: SplashViewModel
 
     override fun onCreateView(
@@ -17,12 +18,20 @@ class SplashFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.splash_fragment, container, false)
+        binding = SplashFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SplashViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        animateLogo()
+    }
+
+    private fun animateLogo() {
+        ObjectAnimator.ofFloat(binding.ivQuizlerLogo, "alpha", 1f).also {
+            it.repeatCount = 1
+            it.duration = 3000
+        }.start()
     }
 }
