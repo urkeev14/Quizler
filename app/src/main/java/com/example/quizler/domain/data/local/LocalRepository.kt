03b4180287a66c868e7adfae54d49c8b7fb60f42
urkeev14 <uros.veljkovic@gmail.com>
2021-06-21 20:15:16 +0200

    Create initial local repository

---
+++ app/src/main/java/com/example/quizler/domain/data/local/LocalRepository.kt
@@ -0,0 +1,33 @@
+package com.example.quizler.domain.data.local
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class LocalRepository(
+    val dataStore: DataStore<Preferences>
+) {
+
+    val token: Flow<String?> = dataStore.data.map { preferences ->
+        preferences[KEY_TOKEN]
+    }
+    val playerId: Flow<String?> = dataStore.data.map { preferences ->
+        preferences[KEY_PLAYER_ID]
+    }
+
+    suspend fun cachePlayerData(playerId: String, token: String) {
+        dataStore.edit { preferences ->
+            preferences[KEY_PLAYER_ID] = playerId
+            preferences[KEY_TOKEN] = token
+        }
+    }
+
+    private companion object {
+        val KEY_PLAYER_ID = stringPreferencesKey("keyPlayerId")
+        val KEY_TOKEN = stringPreferencesKey("keyToken")
+    }
+}
