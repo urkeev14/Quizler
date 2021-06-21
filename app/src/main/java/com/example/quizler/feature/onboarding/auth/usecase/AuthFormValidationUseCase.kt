6c3a2930072a917b0bfed657aa81f0ff6feef1e5
urkeev14 <uros.veljkovic@gmail.com>
2021-06-21 20:16:01 +0200

    Create class used for real-time register/login validation

---
+++ app/src/main/java/com/example/quizler/feature/onboarding/auth/usecase/AuthFormValidationUseCase.kt
@@ -0,0 +1,23 @@
+package com.example.quizler.feature.onboarding.auth.usecase
+
+/**
+ *^                 # start-of-string
+ *(?=.*[0-9])       # a digit must occur at least once
+ *(?=.*[a-z])       # a lower case letter must occur at least once
+ *(?=.*[A-Z])       # an upper case letter must occur at least once
+ *(?=.*[@#$%^&+=])  # a special character must occur at least once
+ *(?=\S+$)          # no whitespace allowed in the entire string
+ *.{8,}             # anything, at least eight places though
+ *$                 # end-of-string
+ */
+private const val REGEX_PASSWORD =
+    "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%!\\-_?&])(?=\\S+\$).{8,}"
+private const val REGEX_USERNAME = "^[a-zA-Z0-9._]{5,}\$"
+private const val REGEX_EMAIL =
+    "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?!-)(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$"
+
+class AuthFormValidationUseCase {
+    fun validateUsername(username: String): Boolean = username.matches(Regex(REGEX_USERNAME))
+    fun validatePassword(password: String): Boolean = password.matches(Regex(REGEX_PASSWORD))
+    fun validateEmail(email: String): Boolean = email.matches(Regex(REGEX_EMAIL))
+}
