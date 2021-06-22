b970d39c20461dcbbc14780f2e28dfd5a8c1ffc4
urkeev14 <uros.veljkovic@gmail.com>
2021-06-22 21:57:10 +0200

    Remove success code when handling register response

--- app/src/main/java/com/example/quizler/feature/onboarding/auth/register/RegisterResponseHandler.kt
+++ app/src/main/java/com/example/quizler/feature/onboarding/auth/register/RegisterResponseHandler.kt
@@ -2,17 +2,15 @@ package com.example.quizler.feature.onboarding.auth.register
 
 import com.example.quizler.R
 
-private const val CODE_SUCCESS = 200
 private const val CODE_ERROR_EMAIL_EXIST = 401
 private const val CODE_ERROR_USERNAME_EXIST = 402
 private const val CODE_ERROR_SERVER_DOWN = 500
 
 class RegisterResponseHandler {
-    fun getRegisterOutcome(responseCode: Int): RegisterOutcome = when (responseCode) {
-        CODE_SUCCESS -> RegisterOutcome(true)
-        CODE_ERROR_EMAIL_EXIST -> RegisterOutcome(false, R.string.error_email_exist)
-        CODE_ERROR_USERNAME_EXIST -> RegisterOutcome(false, R.string.error_username_exist)
-        CODE_ERROR_SERVER_DOWN -> RegisterOutcome(false, R.string.error_server_down)
-        else -> RegisterOutcome(false, R.string.error_unknow)
+    fun getRegisterErrorMessageResId(responseCode: Int): Int = when (responseCode) {
+        CODE_ERROR_EMAIL_EXIST -> R.string.error_email_exist
+        CODE_ERROR_USERNAME_EXIST -> R.string.error_username_exist
+        CODE_ERROR_SERVER_DOWN -> R.string.error_server_down
+        else -> R.string.error_unknow
     }
 }
