65a5ce5c539ec8aaf23914587e06d83d68d096eb
urkeev14 <uros.veljkovic@gmail.com>
2021-06-22 21:57:58 +0200

    Add message resource id to error state for purpose of handling internationalization

--- app/src/main/java/com/example/quizler/util/State.kt
+++ app/src/main/java/com/example/quizler/util/State.kt
@@ -1,10 +1,17 @@
 package com.example.quizler.util
 
+import androidx.annotation.StringRes
+
 sealed class State<T>(
     val data: T? = null,
-    val error: Throwable? = null
+    val error: Throwable? = null,
+    val messageResId: Int? = null
 ) {
     class Success<T>(data: T) : State<T>(data)
     class Loading<T>(data: T? = null) : State<T>(data)
-    class Error<T>(throwable: Throwable? = null, data: T? = null) : State<T>(data, throwable)
+    class Error<T>(
+        throwable: Throwable? = null,
+        data: T? = null,
+        @StringRes messageResId: Int? = null
+    ) : State<T>(data, throwable, messageResId)
 }
