5fb8877a1005895cba5c238db8d66ec29fe03396
urkeev14 <uros.veljkovic@gmail.com>
2021-06-22 21:56:40 +0200

    Move register action state out of binding model class

--- app/src/main/java/com/example/quizler/feature/onboarding/auth/register/RegisterBindingModel.kt
+++ app/src/main/java/com/example/quizler/feature/onboarding/auth/register/RegisterBindingModel.kt
@@ -1,13 +1,8 @@
 package com.example.quizler.feature.onboarding.auth.register
 
-import com.example.quizler.util.Event
-
 data class RegisterBindingModel(
     var isEmailValid: Boolean = false,
     var isEmailAvailable: Boolean = true,
     var isUsernameValid: Boolean = false,
     var isPasswordValid: Boolean = false,
-    val isLoading: Boolean = false,
-    val isRegisterSuccess: Boolean = false,
-    val errorMessage: Event<Int?> = Event(null)
 )
