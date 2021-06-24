dbe6b2c3ffc4f599f53ebc8ae1f8fe887854a263
urkeev14 <uros.veljkovic@gmail.com>
2021-06-24 17:30:46 +0200

    Bind bottom navigation view with nav controller

--- app/src/main/java/com/example/quizler/feature/main/MainActivity.kt
+++ app/src/main/java/com/example/quizler/feature/main/MainActivity.kt
@@ -4,14 +4,31 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.example.quizler.R
+import com.example.quizler.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navHostFragment: NavHostFragment
+    private val navController by lazy { navHostFragment.navController }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 
     companion object {
