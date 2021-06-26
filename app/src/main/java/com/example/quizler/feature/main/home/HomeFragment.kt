ef6f6f8d6b1d0a19c94875153b289efe5029dcef
urkeev14 <uros.veljkovic@gmail.com>
2021-06-26 10:08:43 +0200

    Fix issue when navigating thorugh tabs

--- app/src/main/java/com/example/quizler/feature/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/quizler/feature/main/home/HomeFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import com.example.quizler.R
 import com.example.quizler.databinding.HomeFragmentBinding
 import com.example.quizler.util.extensions.onTabSelected
 import dagger.hilt.android.AndroidEntryPoint
@@ -17,7 +19,8 @@ class HomeFragment : Fragment() {
     private var _binding: HomeFragmentBinding? = null
     private val binding get() = _binding!!
     private val viewModel: HomeViewModel by viewModels()
-    private val navController by lazy { findNavController() }
+    private val navHostFragment by lazy { childFragmentManager.findFragmentById(R.id.nav_host_home) as NavHostFragment }
+    private val navController by lazy { navHostFragment.navController }
 
     override fun onCreateView(
         inflater: LayoutInflater,
