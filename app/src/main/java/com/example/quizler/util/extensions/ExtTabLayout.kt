507c80184a89e376cb975188218355fbeafcd009
urkeev14 <uros.veljkovic@gmail.com>
2021-06-26 09:41:02 +0200

    Create extension function for reacting on tab click

---
+++ app/src/main/java/com/example/quizler/util/extensions/ExtTabLayout.kt
@@ -0,0 +1,18 @@
+package com.example.quizler.util.extensions
+
+import com.google.android.material.tabs.TabLayout
+
+fun TabLayout.onTabSelected(callback: (Int) -> Unit) {
+    this.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+        override fun onTabSelected(tab: TabLayout.Tab?) {
+            callback(tab!!.position)
+        }
+
+        override fun onTabUnselected(tab: TabLayout.Tab?) {
+        }
+
+        override fun onTabReselected(tab: TabLayout.Tab?) {
+        }
+
+    })
+}
