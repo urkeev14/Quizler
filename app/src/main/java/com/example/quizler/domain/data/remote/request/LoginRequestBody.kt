693d1e80fce7a78eb62f922ae199032898b58acf
urkeev14 <uros.veljkovic@gmail.com>
2021-07-01 15:53:07 +0200

    Set default starting activity back to Onboarding and remove unused import

--- app/src/main/java/com/example/quizler/domain/data/remote/request/LoginRequestBody.kt
+++ app/src/main/java/com/example/quizler/domain/data/remote/request/LoginRequestBody.kt
@@ -1,7 +1,5 @@
 package com.example.quizler.domain.data.remote.request
 
-import com.google.gson.annotations.SerializedName
-
 data class LoginRequestBody(
     val username: String,
     val password: String,
