11d4bdaf7aacd1cb5e17c5d54a943b7a03aa71fd
urkeev14 <uros.veljkovic@gmail.com>
2021-07-05 18:25:40 +0200

    Create mock data provider for temporary use until server endpoints are implemented

---
+++ app/src/main/java/com/example/quizler/feature/main/home/quiz_mode/QuizModeMockDataProvider.kt
@@ -0,0 +1,60 @@
+package com.example.quizler.feature.main.home.quiz_mode
+
+import com.example.quizler.domain.model.QuizModeDto
+
+class QuizModeMockDataProvider {
+
+    private val listDifficulty: List<QuizModeDto> = mutableListOf(
+        QuizModeDto(
+            id = "111",
+            name = "easy",
+            timePerQuestion = 30,
+            numberOfHints = 3,
+            numberOfQuestions = 20
+        ),
+        QuizModeDto(
+            id = "111",
+            name = "average",
+            timePerQuestion = 30,
+            numberOfHints = 3,
+            numberOfQuestions = 20
+        ),
+        QuizModeDto(
+            id = "111",
+            name = "hard",
+            timePerQuestion = 30,
+            numberOfHints = 3,
+            numberOfQuestions = 20
+        )
+    )
+
+    private val listLength: List<QuizModeDto> = mutableListOf(
+        QuizModeDto(
+            id = "111",
+            name = "zen",
+            timePerQuestion = 30,
+            numberOfHints = 3,
+            numberOfQuestions = 20
+        ),
+        QuizModeDto(
+            id = "111",
+            name = "exam",
+            timePerQuestion = 30,
+            numberOfHints = 3,
+            numberOfQuestions = 20
+        ),
+        QuizModeDto(
+            id = "111",
+            name = "marathon",
+            timePerQuestion = 30,
+            numberOfHints = 3,
+            numberOfQuestions = 20
+        )
+    )
+
+    val mockedModes = mapOf(
+        "tezina" to listDifficulty,
+        "duzina" to listLength
+    )
+
+}
