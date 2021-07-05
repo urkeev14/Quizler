6deaffca1fc6cce8bf30de070a3e3730de83f006
urkeev14 <uros.veljkovic@gmail.com>
2021-07-05 18:36:37 +0200

    MIgrate creation of mock quiz items from adatper to QuizModeMockDataProvider.kt

--- app/src/main/java/com/example/quizler/feature/main/home/quiz_mode/QuizItemComplexAdapter.kt
+++ app/src/main/java/com/example/quizler/feature/main/home/quiz_mode/QuizItemComplexAdapter.kt
@@ -4,33 +4,11 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.quizler.databinding.ListItemQuizComplexBinding
-import com.example.quizler.domain.model.QuizModeItem
+import com.example.quizler.domain.model.QuizMode
 
-class QuizItemComplexAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-
-    private val list: MutableList<QuizModeItem> = mutableListOf(
-        QuizModeItem(
-            id = "111",
-            name = "zen",
-            timePerQuestion = 30,
-            numberOfHints = 3,
-            numberOfQuestions = 20
-        ),
-        QuizModeItem(
-            id = "111",
-            name = "test",
-            timePerQuestion = 30,
-            numberOfHints = 3,
-            numberOfQuestions = 20
-        ),
-        QuizModeItem(
-            id = "111",
-            name = "maraton",
-            timePerQuestion = 30,
-            numberOfHints = 3,
-            numberOfQuestions = 20
-        )
-    )
+class QuizItemComplexAdapter(
+    private val list: List<QuizMode>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val binding = ListItemQuizComplexBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -46,7 +24,7 @@ class QuizItemComplexAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun getItemCount(): Int = list.size
 
     class QuizItemComplexViewHolder(var binding: ListItemQuizComplexBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: QuizModeItem) {
+        fun bind(item: QuizMode) {
             binding.item = item
         }
     }
