afc72b612d84cf1f1866a699aa5825116c5659b5
urkeev14 <uros.veljkovic@gmail.com>
2021-07-16 09:23:21 +0200

    Refactor lint warning

--- app/src/main/java/com/example/quizler/di/viewmodel/RegisterModule.kt
+++ app/src/main/java/com/example/quizler/di/viewmodel/RegisterModule.kt
@@ -13,9 +13,6 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
-import dagger.hilt.android.scopes.ViewModelScoped
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
